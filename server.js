456db61ca9323b2420bbbcb4bb1a4653d722787e
Roshan Justin <77222525+roshanjustin0010@users.noreply.github.com>
2024-01-15 18:11:32 +0530

    completed api pending update

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+require('dotenv').config()
+
 const express = require("express")
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/subscribers')
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error',(error) => console.error(error))
 db.on('open',() => console.log('connected to database successfully'))
 
+app.use(express.json())
 
-app.listen(3000, () => console.log('server started rose !!!'))
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers',subscribersRouter)
+
+
+app.listen(8080, () => console.log('server started rose !!!'))
